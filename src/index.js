9add36c535c7241efc505b0eff99de904d7ec72e
nduhiu17 <nduhiu254@gmail.com>
2020-04-30 09:30:04 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'
+import cartReducer from './components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import thunk from "redux-thunk";
+import {applyMiddleware, compose} from "redux";
+
+const initialState = {
+    items: [],
+    addedItems:[],
+    total: 0
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+    cartReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
