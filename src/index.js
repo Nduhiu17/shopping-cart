11340aa55cf0004f05502a176b47622338cf9ba8
nduhiu17 <nduhiu254@gmail.com>
2020-04-30 13:35:26 +0300

    remove redux chrome extention

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ const store = createStore(
     cartReducer,
     initialState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
     )
 );
 
